bef45ca0231da67a74e63189bca5aa3706779115
pn101 <philipnam101@gmail.com>
2016-06-22 23:00:54 +0900

    init staticfiles_dirs: added stylesheets to header and footer

--- fastvid/fastvid/settings/partials/static.py
+++ fastvid/fastvid/settings/partials/static.py
@@ -1,9 +1,12 @@
 import os
 from .base import PROJECT_ROOT_DIR
+from .base import BASE_DIR
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'fastvid', 'static',)]
 
 STATIC_URL = '/static/'
 
