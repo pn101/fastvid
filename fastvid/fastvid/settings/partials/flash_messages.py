921c4c21d1c997a51aff60f21eaa0ed8cf58311b
pn101 <philipnam101@gmail.com>
2016-06-22 22:46:39 +0900

    add flash_messages for LoginView

--- fastvid/fastvid/settings/partials/flash_messages.py
+++ fastvid/fastvid/settings/partials/flash_messages.py
@@ -2,4 +2,7 @@
 
 # Signup, Login, Logout
 
+LOGIN_SUCCESS_MESSAGE = 'Successfully logged in'
 LOGOUT_SUCCESS_MESSAGE = 'Successfully logged out'
+
+LOGIN_ERROR_MESSAGE = 'Failed to log in. Please try again'
--- fastvid/users/views/login.py
+++ fastvid/users/views/login.py
@@ -1,6 +1,8 @@
 from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.conf import settings
 
 
 class LoginView(View):
@@ -26,5 +28,17 @@ class LoginView(View):
 
         if user:
             login(request, user)
+
+            messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    settings.LOGIN_SUCCESS_MESSAGE,
+            )
             return redirect(next_page)
+
+        messages.add_message(
+                request,
+                messages.ERROR,
+                settings.LOGIN_ERROR_MESSAGE,
+        )
         return redirect('user:login')
