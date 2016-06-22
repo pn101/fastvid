5f19a38c5fedd0366d385c5854ff2980d1d978a2
pn101 <philipnam101@gmail.com>
2016-06-22 22:31:46 +0900

    init LogoutView

--- fastvid/users/urls/__init__.py
+++ fastvid/users/urls/__init__.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^signup/$', SignupView.as_view(), name='signup'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^mypage/$', MyPageView.as_view(), name='mypage'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
 ]
