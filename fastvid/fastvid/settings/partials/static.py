cbd4df2234e082557d62f355817cd822fb089d68
pn101 <philipnam101@gmail.com>
2016-06-22 23:16:36 +0900

    add pipeline app: update staticfiles settings

--- fastvid/fastvid/settings/partials/static.py
+++ fastvid/fastvid/settings/partials/static.py
@@ -10,6 +10,33 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'fastvid', 'static',)]
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(
+        PROJECT_ROOT_DIR,
+        'dist',
+        'static',
+)
+
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'pipeline.finders.PipelineFinder',
+)
+
+PIPELINE = {
+    'PIPELINE_ENABLED': False,
+    'STYLESHEETS': {
+        'fastvid': {
+            'source_filenames': (
+              'stylesheets/main.css',
+              'stylesheets/partials/*.css',
+            ),
+            'output_filename': 'stylesheets/fastvid.css',
+        }
+    }
+}
+
 # Media Files
 MEDIA_ROOT = os.path.join(
         PROJECT_ROOT_DIR,
