1615a3e5724594cfc4b03fad0b1229e2700919d6
pn101 <philipnam101@gmail.com>
2016-06-23 19:54:03 +0900

    add SOCIAL_AUTH_PIPELINE override: conflicted with PIPELINE

--- fastvid/fastvid/settings/partials/social_auth.py
+++ fastvid/fastvid/settings/partials/social_auth.py
@@ -11,3 +11,16 @@ SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')
 
 SOCIAL_AUTH_REDIRECT_LOGIN = '/mypage/'
+
+SOCIAL_AUTH_PIPELINE = (
+        'social.pipeline.social_auth.social_details',
+        'social.pipeline.social_auth.social_uid',
+        'social.pipeline.social_auth.auth_allowed',
+        'social.pipeline.social_auth.social_user',
+        'social.pipeline.user.get_username',
+        'social.pipeline.social_auth.associate_by_email',
+        'social.pipeline.user.create_user',
+        'social.pipeline.social_auth.associate_user',
+        'social.pipeline.social_auth.load_extra_data',
+        'social.pipeline.user.user_details',
+)
