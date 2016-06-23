9a05a504547a05ba4adf33e702687eaf1ec141ca
pn101 <philipnam101@gmail.com>
2016-06-23 19:44:49 +0900

    add SOCIAL_AUTH_FACEBOOK_KEY and SOCIAL_AUTH_FACEBOOK_SECRET

--- fastvid/fastvid/settings/partials/social_auth.py
+++ fastvid/fastvid/settings/partials/social_auth.py
@@ -1,5 +1,11 @@
+import os
+
+
 AUTHENTICATION_BACKENDS = [
     'social.backends.facebook.FacebookOAuth2',
 
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
+SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')
