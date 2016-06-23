ec85e44bcdb958c3f14cc8c1d6a68f10bbf76dc2
pn101 <philipnam101@gmail.com>
2016-06-23 22:47:36 +0900

    init PostCreateConfirmView

--- fastvid/posts/urls/__init__.py
+++ fastvid/posts/urls/__init__.py
@@ -5,6 +5,7 @@ from posts.views import *
 
 urlpatterns = [
     url(r'^create/$', PostCreateView.as_view(), name='create'),
+    url(r'^confirm/$', PostCreateConfirmView.as_view(), name='confirm'),
     url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
 
 ]
--- fastvid/posts/views/create.py
+++ fastvid/posts/views/create.py
@@ -1,6 +1,8 @@
 from django.views.generic import View
 from django.shortcuts import render, redirect
 
+from posts.utils import youtube
+
 
 class PostCreateView(View):
 
@@ -28,4 +30,22 @@ class PostCreateView(View):
 
 
 class PostCreateConfirmView(View):
-    pass
+
+    def post(self, request, *args, **kwargs):
+        title = request.POST.get('title')
+        video_id = request.POST.get('video_id')
+        content = request.POST.get('content')
+
+        return render(
+                request,
+                'post/create_confirm.html',
+                {
+                    'site_name': 'Confirm Post',
+                    'title': title,
+                    'video_id': video_id,
+                    'content': content,
+
+                    'youtube_original_url': youtube.get_youtube_original_url(video_id),
+                    'youtube_embed_url': youtube.get_youtube_embed_url(video_id),
+                }
+        )
