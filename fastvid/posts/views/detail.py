9d631555e5d22c7f3eecc8e4783094ae71a0d2e0
pn101 <philipnam101@gmail.com>
2016-06-25 15:16:10 +0900

    update PostDetailView: remove unecessary context_object_name

--- fastvid/posts/views/detail.py
+++ fastvid/posts/views/detail.py
@@ -5,7 +5,6 @@ from .base import PostBaseView
 
 class PostDetailView(PostBaseView, DetailView):
     template_name = 'post/detail.html'
-    context_object_name = 'post'
 
     def get_context_data(self, **kwargs):
         context = super(PostDetailView, self).get_context_data(**kwargs)
