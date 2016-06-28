8090e04f0d67e8b668edcd32a77f34b2e1747fea
pn101 <philipnam101@gmail.com>
2016-06-28 16:36:24 +0900

    update posts.js: add temp add comment code

--- fastvid/fastvid/static/js/posts.js
+++ fastvid/fastvid/static/js/posts.js
@@ -11,12 +11,22 @@ var main = function() {
             $('.comment-count').html(commentCount);
 
             data.forEach(function(comment) {
-                var newCommentLi = $('<li>').text(comment.content);
-                $('.comment-list').append(newCommentLi);
+                var CommentLi = $('<li>').text(comment.content);
+                $('.comment-list').append(CommentLi);
             });
         },
         error: function(data) {},
-    })
+    });
+
+    var commentBox = $('.comment-content').find('input[name="content"]');
+    
+    $('.comment-content').submit(function() {
+        var content = $(commentBox).val();
+        $(commentBox).val('');
+        var newCommentLi = $('<li>').text(content);
+        $('.comment-list').append(newCommentLi);
+        return false;
+    });
 };
 
 $(document).ready(main)
