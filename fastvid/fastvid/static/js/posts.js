3e7940d1a34d95936eefca1c43676c276ee3aa8b
pn101 <philipnam101@gmail.com>
2016-06-28 15:51:54 +0900

    init ajax comment count and list

--- fastvid/fastvid/static/js/posts.js
+++ fastvid/fastvid/static/js/posts.js
@@ -1,5 +1,22 @@
 var main = function() {
-    alert('Hi');
+    var postSlug = $('#comment-section').data('post-slug');
+    var commentAPIURL = '/api/posts/' + postSlug + '/comments/';    
+    alert(commentAPIURL);
+
+    $.ajax({
+        url: commentAPIURL,
+        type: 'GET',
+        success: function(data) {
+            var commentCount = data.length;
+            $('.comment-count').html(commentCount);
+
+            data.forEach(function(comment) {
+                var newCommentLi = $('<li>').text(comment.content);
+                $('.comment-list').append(newCommentLi);
+            });
+        },
+        error: function(data) {},
+    })
 };
 
 $(document).ready(main)
