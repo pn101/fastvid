d868efb58266586c0ae97470792b2fa3f97619dd
pn101 <philipnam101@gmail.com>
2016-06-28 16:51:36 +0900

    update Tag Model

--- fastvid/tags/models/tag.py
+++ fastvid/tags/models/tag.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from users.models import User
+from posts.models import Post
 
 
 class Tag(models.Model):
@@ -8,6 +8,8 @@ class Tag(models.Model):
     name = models.CharField(
             max_length=256,
     )
+
+    post_set = models.ManyToManyField(Post)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
