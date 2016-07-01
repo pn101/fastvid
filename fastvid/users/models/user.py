7ceb1c122a298691cc5196c4d66626ead24c6c6d
pn101 <philipnam101@gmail.com>
2016-07-01 17:48:38 +0900

    init Follow Intermediary Model:
    User to User follow model

--- fastvid/users/models/user.py
+++ fastvid/users/models/user.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .follow import Follow
+
 
 class User(AbstractUser):
 
     phonenumber = models.CharField(
             max_length=16,
+    )
+
+    follower_set = models.ManyToManyField(
+            'self',
+            symmetrical=False,
+            related_name='following_set',
+            through=Follow,
+            through_fields=('following', 'follower'),
     )
