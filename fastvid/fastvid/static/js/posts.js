18e3a062b6218d8a0a61ec4d9f9e2fc9e2102b24
pn101 <philipnam101@gmail.com>
2016-07-01 19:06:37 +0900

    init ajax comment add and count

--- fastvid/fastvid/static/js/posts.js
+++ fastvid/fastvid/static/js/posts.js
@@ -1,4 +1,34 @@
 var main = function() {
+    // using jQuery
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
     var postSlug = $('#comment-section').data('post-slug');
     var commentAPIURL = '/api/posts/' + postSlug + '/comments/';    
 
@@ -15,15 +45,38 @@ var main = function() {
             });
         },
         error: function(data) {},
-    });
+    })
 
     var commentBox = $('.comment-content').find('input[name="content"]');
     
     $('.comment-content').submit(function() {
         var content = $(commentBox).val();
-        $(commentBox).val('');
-        var newCommentLi = $('<li>').text(content);
-        $('.comment-list').append(newCommentLi);
+        var data = {
+            content: content,
+        }
+
+        $.ajax({
+            url: commentAPIURL,
+            type: 'POST',
+            data: data,
+            success: function(data) {
+                var commentContent = data.content;
+                var commentUsername = data.username;
+
+                // add comment
+                var newCommentLi = $('<li>').text(content);
+                $('.comment-list').append(newCommentLi);
+
+                // update comment count
+                var commentCount = $('.comment-count').html();
+                var newCommentCount = Number(commentCount) + 1;
+                $('.comment-count').html(String(newCommentCount));
+
+                $(commentBox).val('');
+
+            },
+            error: function(data) {},
+        })
         return false;
     });
 };
