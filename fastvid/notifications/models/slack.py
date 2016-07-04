aee174024ac2b51427ad098ff5ab2229946f1a1b
pn101 <philipnam101@gmail.com>
2016-07-04 22:59:55 +0900

    SlackNofitication and SMSNotification Models to inherit BaseNotification Abstract Model

---
+++ fastvid/notifications/models/slack.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from .base import BaseNotification
+
+
+class SlackNotification(BaseNotification):
+
+    class Meta:
+        verbose_name = 'Slack Notification'
+        verbose_name_plural = verbose_name
---
+++ fastvid/notifications/models/sms.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from .base import BaseNotification
+
+
+class SMSNotification(BaseNotification):
+
+    class Meta:
+        verbose_name = 'SMS Notification'
+        verbose_name_plural = verbose_name
