3ef99fdc4276311e627efec296513e322bb1a413
pn101 <philipnam101@gmail.com>
2016-07-05 19:23:16 +0900

    add fastvid postgresql DATABASE_URL

--- fastvid/fastvid/settings/partials/database.py
+++ fastvid/fastvid/settings/partials/database.py
@@ -5,9 +5,18 @@ from .base import BASE_DIR
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# dj-database-url
+# https://github.com/kennethreitz/dj-database-url
+import dj_database_url
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+)
